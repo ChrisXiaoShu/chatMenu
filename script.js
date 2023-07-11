a4614ca5025699e26b7ca3cea4179c42b53f115f
ChrisXiaoShu <gah8974@gmail.com>
2023-07-11 17:27:06 +0800

    feat(start-button): add start button to start chat

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
         var diamondContainer = document.getElementById("diamond-container");
         var contentHeading = document.getElementById("content-heading");
         var contentText = document.getElementById("content-text");
+        var startBtn = document.getElementById("start-btn");
 
         diamondContainer.style.animation = "none";
         diamondContainer.offsetHeight; // Force reflow to reset animation
@@ -11,7 +12,12 @@ document.addEventListener("DOMContentLoaded", function() {
         diamondContainer.classList.add("hidden");
         contentHeading.classList.remove("hidden");
         contentText.classList.remove("hidden");
-        contentHeading.style.animation = "fadeIn 3s ease-in-out";
-        contentText.style.animation = "fadeIn 5s ease-in-out";
+        contentHeading.style.animation = "fadeIn 1s ease-in-out";
+        contentText.style.animation = "fadeIn 1s ease-in-out";
+
+        setTimeout(function() {
+            startBtn.classList.remove("hidden");
+            startBtn.style.animation = "fadeIn 1s ease-in-out";
+        }, 1000); // Delay the display of the start button by 1 second
     }, 1000);
 });
