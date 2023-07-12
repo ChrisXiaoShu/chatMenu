8aa7b240a8504696921d762cd48b673e2010f3c2
ChrisXiaoShu <gah8974@gmail.com>
2023-07-12 14:36:05 +0800

    fix(start-btn): hide start button after click
    1. hide button after click
    2. to a function

--- script.js
+++ script.js
@@ -9,6 +9,42 @@ function formatText(text) {
     return formattedText;
 }
 
+function startButtonClick() {
+  // Hide the start button
+  const startBtn = document.getElementById("start-btn");
+  startBtn.style.display = "none";
+  var contentHeading = document.getElementById("content-heading");
+  var contentText = document.getElementById("content-text");
+  contentHeading.innerHTML = "";
+  contentText.innerHTML = "Loading...";
+
+  fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + apiKey
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "system", content: prompt },
+        { role: "user", content: prompt }
+      ]
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      const answer = data.choices[0].message.content.trim();
+      contentText.innerHTML = formatText(answer);
+    })
+    .catch(error => {
+      console.error("Error:", error);
+      contentText.innerHTML = "Error occurred. Please try again.";
+    });
+}
+
+
+// Welcome start animation
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         var diamondContainer = document.getElementById("diamond-container");
@@ -25,40 +61,9 @@ document.addEventListener("DOMContentLoaded", function() {
         contentText.classList.remove("hidden");
         contentHeading.style.animation = "fadeIn 1s ease-in-out";
         contentText.style.animation = "fadeIn 1s ease-in-out";
-
         setTimeout(function() {
             startBtn.classList.remove("hidden");
             startBtn.style.animation = "fadeIn 1s ease-in-out";
         }, 1000); // Delay the display of the start button by 1 second
-
-        startBtn.addEventListener("click", function() {
-            contentHeading.innerHTML = "";
-            contentText.innerHTML = "Loading...";
-
-            fetch("https://api.openai.com/v1/chat/completions", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer " + apiKey
-              },
-              body: JSON.stringify({
-                model: "gpt-3.5-turbo",
-                messages: [
-                  { role: "system", content: prompt },
-                  { role: "user", content: prompt }
-                ]
-              })
-            })
-              .then(response => response.json())
-              .then(data => {
-                const answer = data.choices[0].message.content.trim();
-                contentText.innerHTML = formatText(answer);
-              })
-              .catch(error => {
-                console.error("Error:", error);
-                contentText.innerHTML = "Error occurred. Please try again.";
-              });
-          });
-          
     }, 1000);
 });
